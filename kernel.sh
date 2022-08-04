3b65cc5966431ace2b5114f9defedbb728b951e1
fcanlnony <76856769+fcanlnony@users.noreply.github.com>
2022-08-04 16:17:52 +0800

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -2,6 +2,5 @@
 emerge sys-kernel/gentoo-kernel
 emerge sys-kernel/gentoo-kernel-bin
 eselect kernel set 1
-wget https://github.com/glacion/genfstab/blob/master/genfstab
-chmod +x genfstab
-./genfstab -U / >> /etc/fstab
+emerge sys-fs/genfstab
+genfstab -U / >> /etc/fstab
